0dd3baf5c6040aea3b436eae9e14acf9543f197b
kowal319 <bartlomiej319@wp.pl>
2022-09-20 17:27:28 +0200

    article model

---
+++ src/main/java/models/Article.java
@@ -0,0 +1,38 @@
+package models;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Article {
+
+private static final int MIN_TITLE_LENGTH = 7;
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id")
+private Long id;
+
+@Column(name = "title")
+private String title;
+
+@Column(name = "text")
+private String text;
+
+@CreationTimestamp
+@Temporal(TemporalType.TIMESTAMP)
+@Column(name = "creation_date")
+private Date creationDate;
+    }
