257e5c3938655cbb9921d50020076af4866ce176
kowal319 <bartlomiej319@wp.pl>
2022-09-30 14:21:09 +0200

    getArticle by id controller done - backend

--- src/main/java/com/example/blogg/controllers/ArticleController.java
+++ src/main/java/com/example/blogg/controllers/ArticleController.java
@@ -5,6 +5,8 @@ import com.example.blogg.services.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,11 +20,16 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
-    @GetMapping("/articles")
-    public String getArticles(Model model, Article article){
-        List<Article> articles = articleService.getAllArticles(article);
-        model.addAttribute("articles", articles);
-        return "articles";
+    @GetMapping("/article/{id}")
+    @ResponseBody
+    public Article getArticle(@PathVariable Long id){
+        return articleService.getArticleById(id);
     }
 
+    @GetMapping("article/details/{id}")
+    public String detailsArticle(@PathVariable Long id, Model model){
+        Article article = articleService.getArticleById(id);
+        model.addAttribute("article", article);
+        return "article";
+    }
 }
